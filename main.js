513b8e81d00cb947b067f2251c0f6200b754abe2
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-06 12:54:42 -0400

    feat: Add basic math functions

---
+++ main.js
@@ -0,0 +1,39 @@
+// Memory
+let gValueX = 0;
+let gOperator = "";
+let gValueY = 0;
+
+// Add
+const add = (addendX, addendY) => {
+    return addendX + addendY;
+}
+
+// Subtract
+const subtract = (minuend, subtrahend) => {
+    return minuend - subtrahend;
+}
+
+// Multiply
+const multiply = (factorX, factorY) => {
+    return factorX * factorY;
+}
+
+// Divide
+const divide = (dividendX, dividendY) => {
+    return dividendX / dividendY;
+}
+
+const operate = (operator, x, y) => {
+    switch (operator) {
+        case '+':
+            return add(x,y);
+        case '-':
+            return subtract(x,y);
+        case '*':
+            return multiply(x,y);
+        case '/':
+            return divide(x,y);
+        default:
+            return "Improper Input";
+    }
+}
